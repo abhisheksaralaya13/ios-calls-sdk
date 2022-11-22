a540f29120b4eec0f9e6f65d853883aff943a90e
abhishek.cometchat <78895566+abhisheksaralaya13@users.noreply.github.com>
2022-11-23 02:22:03 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,19 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        .library(name: "CometChatProCalls", targets: ["CometChatProCalls"])
+        .library(name: "CometChatProCalls", targets: ["CometChatProCalls","WebRTC"])
     ],
     targets: [
         .binaryTarget(
             name: "CometChatProCalls",
             url: "https://library.cometchat.io/ios/v2.1/xcode12/CometChatProCalls.xcframework.zip",
             checksum: "ee4b722abda40ab4a8d77700ff4998910b6f3466cf600852ed58751278ab5edc"
+        ),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://library.cometchat.io/ios/v2.0/xcode12/WebRTC.xcframework.zip",
+            checksum: "9141e5978496217b30c44e09bba2845372a38445a17f752ff96b5977295b1b33"
         )
     ]
+    
 )
